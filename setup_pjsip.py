172d2917f6e2e1b7442acb366ffbd8eb5de60da9
Eric Lafontaine <eric.lafontaine1@gmail.com>
2017-02-21 10:46:55 -0500

    Update setup_pjsip.py

--- setup_pjsip.py
+++ setup_pjsip.py
@@ -145,7 +145,7 @@ class PJSIP_build_ext(build_ext):
     def configure_pjsip(self):
         log.info("Configuring PJSIP")
         with open(os.path.join(self.build_dir, "pjlib", "include", "pj", "config_site.h"), "wb") as f:
-            f.write("\n".join(self.config_site+[""]))
+            f.write(b"\n".join(bytes(self.config_site)+[b""]))
         cflags = "-DNDEBUG -g -fPIC -fno-omit-frame-pointer -fno-strict-aliasing -Wno-unused-label"
         if self.debug or hasattr(sys, 'gettotalrefcount'):
             log.info("PJSIP will be built without optimizations")
